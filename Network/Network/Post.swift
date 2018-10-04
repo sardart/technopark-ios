6c606a3e42cf553af6f4fce1c706e1a45bb26c72
Artur Sardaryan <ardart@bk.ru>
2018-10-04 13:36:06 +0300

    Uploaded code

---
+++ Network/Network/Post.swift
@@ -0,0 +1,42 @@
+//
+//  Post.swift
+//  Network
+//
+//  Created by Artur on 04/10/2018.
+//  Copyright © 2018 Artur Sardaryan. All rights reserved.
+//
+
+import Foundation
+
+
+struct Post {
+    
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+
+}
+
+
+extension Post {
+    
+    init?(dict: NSDictionary) {
+        guard
+        let userId = dict["userId"] as? Int,
+        let id = dict["id"] as? Int,
+        let title = dict["title"] as? String,
+        let body = dict["body"] as? String
+            else { return nil }
+
+        self.userId = userId
+        self.id = id
+        self.title = title
+        self.body = body
+    }
+    
+    
+}
+
+
+
---
+++ Network/Network/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Network
+//
+//  Created by Artur on 04/10/2018.
+//  Copyright © 2018 Artur Sardaryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var posts = [Post]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        getPosts()
+    }
+    
+    func getPosts() {
+        let urlString = "https://jsonplaceholder.typicode.com/posts"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            guard let data = data else { return }
+        
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                self.parsePosts(from: json)
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+            }.resume()
+    }
+    
+    func parsePosts(from json: Any) {
+        guard let postsArray = json as? NSArray else {
+            print("Parse error")
+            return
+        }
+        
+        var tmp = [Post]()
+        for i in postsArray {
+            guard let postDict = i as? NSDictionary,
+                let post = Post(dict: postDict) else { continue }
+            tmp.append(post)
+        }
+        self.posts = tmp
+        print(posts)
+    }
+    
+    
+}
+

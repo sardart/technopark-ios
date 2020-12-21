c8acaf9a2b5ce3316e98dcfde5fda5989d5f9ee5
Pavel Nosov <p.nosov@corp.mail.ru>
2020-12-21 04:17:14 +0300

    Add podspec

---
+++ Autumn-2020/13/Framework/testpod/Pods/Target Support Files/Pods-testpod/Pods-testpod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testpod : NSObject
+@end
+@implementation PodsDummy_Pods_testpod
+@end
---
+++ Autumn-2020/13/Framework/testpod/testpod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testpod
+//
+//  Created by p.nosov on 21.12.2020.
+//
+
+import UIKit
+import tutorialpd
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let gameCharacted = GameCharacter(name: "")
+    }
+
+
+}
+
